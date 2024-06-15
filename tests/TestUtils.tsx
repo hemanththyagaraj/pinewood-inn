447c0eafcef1a7527f67a555c34ec94cfd929471
Hemanth <hemanththyagaraj@gmail.com>
2024-06-15 23:40:27 +0530

    feat: setup unit test for testing components

---
+++ tests/TestUtils.tsx
@@ -0,0 +1,12 @@
+import { RenderOptions, render } from '@testing-library/react';
+import React from 'react';
+import AllProviders from './AllProvider';
+
+const customRender = (ui: React.ReactNode, options?: RenderOptions) => {
+  return render(ui, { wrapper: AllProviders, ...options });
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export * from '@testing-library/react';
+
+export { customRender as render };
