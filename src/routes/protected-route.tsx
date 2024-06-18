ff5f73af7b47520ccd2ea3b06cf873771f7e2640
Hemanth <hemanththyagaraj@gmail.com>
2024-06-18 23:52:55 +0530

    feat: Setup router to handle navigating through pages

---
+++ src/routes/protected-route.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = (props: { children: ReactNode }) => {
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return props.children;
+};
+
+export default ProtectedRoute;
