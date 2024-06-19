23acaae9d2a421d502859ef68ba4f28dd4e674a7
Hemanth <hemanththyagaraj@gmail.com>
2024-06-19 15:41:04 +0530

    feat: setup sidebar component with test case

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  width: 30vw;
+`;
+
+const Sidebar = () => {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+};
+
+export default Sidebar;
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./tests/setup.ts'],
+    setupFiles: ['./src/tests/setup.ts'],
   },
 });
